17de6e5135261d58818d2e59efc2bd8b44d72ab7
Hanjaemo <hjm2530@gmail.com>
2023-01-22 22:14:43 +0900

    PickTwoAddThem solve

--- week1/B2587.java
+++ week1/B2587.java
@@ -14,12 +14,22 @@ class B2587 {
         System.out.println(getMedian(numbers));
     }
 
+    /**
+     * 배열의 길이만큼 숫자를 입력받고 배열에 저장한다.
+     * @param arrayNum
+     * @param scanner
+     */
     static void append(int[] arrayNum, Scanner scanner){
         for (int i=0; i<arrayNum.length; i++) {
             arrayNum[i] = Integer.parseInt(scanner.nextLine());
         }
     }
 
+    /**
+     * 배열의 요소들의 평균을 반환한다.
+     * @param numbers
+     * @return 배열의 모든 요소의 평균
+     */
     static int getAverage(int[] numbers) {
         int total = 0;
         for (int i=0; i<numbers.length; i++) {
@@ -29,6 +39,11 @@ class B2587 {
         return total / numbers.length;
     }
 
+    /**
+     * 배열을 정렬하고, 중앙값을 반환한다.
+     * @param numbers
+     * @return 배열의 중앙에 있는 요소
+     */
     static int getMedian(int[] numbers) {
         Arrays.sort(numbers);
         return numbers[2];
