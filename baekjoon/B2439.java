235b0b4e422dcf4c248a930c97a2e9f5169c29e7
Hanjaemo <hjm2530@gmail.com>
2023-02-01 22:38:02 +0900

    solve

---
+++ baekjoon/B2439.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+class B2439 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+
+        for (int i=1; i<=N; i++) {
+            for (int j=i; j<N; j++) {
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
