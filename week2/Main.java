49811d4e3aca1940b4172aca7080b61eab806050
Hanjaemo <hjm2530@gmail.com>
2023-02-03 15:56:43 +0900

    solve

--- week2/Main.java
+++ week2/Main.java
@@ -2,14 +2,12 @@ package week2;
 
 import java.util.Scanner;
 
-public class B1546 {
+class Main {
     private static Scanner scanner = new Scanner(System.in);
     private static int nSubject;
     private static int[] scoreArr;
     private static int maxScore;
-    private static float newScore;
-
-    private static float[] newScoreArr;
+    private static double[] newScoreArr;
 
     /**
      * logic
@@ -32,7 +30,7 @@ public class B1546 {
             nSubject = Integer.parseInt(scanner.nextLine());
         }
 
-        B1546 b1546 = new B1546();
+        Main b1546 = new Main();
 
         scoreArr = b1546.setScoreArr();
         while (!validation.validateIsZero(scoreArr)) {
@@ -47,12 +45,12 @@ public class B1546 {
 
         b1546.setNewScoreArr();
         System.out.println(b1546.getAvg());
-
-
-
-
     }
 
+    /**
+     * 점수를 입력 받아 배열에 저장하는 메서드
+     * @return
+     */
     public int[] setScoreArr() {
         int[] scoreArr = new int[nSubject];
 
@@ -63,6 +61,10 @@ public class B1546 {
         return scoreArr;
     }
 
+    /**
+     * 점수들 중 최댓값을 반환하는 메서드
+     * @return
+     */
     public int getMaxScore() {
         maxScore = 0;
         for (int i=0; i<scoreArr.length; i++) {
@@ -73,13 +75,20 @@ public class B1546 {
         return maxScore;
     }
 
+    /**
+     * 기존 배열의 요소(점수)를 최댓값으로 나누고 100을 곱하여 새로운 배열을 만든다.
+     */
     public void setNewScoreArr() {
-        newScoreArr = new float[scoreArr.length];
+        newScoreArr = new double[scoreArr.length];
         for(int i=0; i<scoreArr.length; i++) {
-            newScoreArr[i] = (float) scoreArr[i] / getMaxScore() * 100;
+            newScoreArr[i] = (double) scoreArr[i] / getMaxScore() * 100;
         }
     }
 
+    /**
+     * 새로 완성된 배열의 모든 요소의 평균을 구하는 메서드
+     * @return
+     */
     public float getAvg() {
         int total = 0;
         for (int i=0; i<newScoreArr.length; i++) {
@@ -92,14 +101,19 @@ public class B1546 {
 class Validation {
     /**
      * 과목 개수의 유효성 검사하는 메서드
-     * @param nSubject 입력 받을 과목의 개수
-     * @return nSubject가 0~1000 사이의 정수인지 확인하고 반환한다.
+     * @param nSubject
+     * @return
      */
     public boolean validateNSubject(int nSubject) {
         if (0<=nSubject && nSubject <=1000) return true;
         else return false;
     }
 
+    /**
+     * 점수가 0~100 사이의 정수인지 확인하는 메서드
+     * @param scoreArr
+     * @return
+     */
     public boolean invalidScore(int[] scoreArr) {
         for (int i=0; i<scoreArr.length; i++) {
             if (!(0<=scoreArr[i] && scoreArr[i]<=100)) {
@@ -109,6 +123,11 @@ class Validation {
         return false;
     }
 
+    /**
+     * 적어도 하나 이상의 점수가 0보다 큰지 확인하는 메서드
+     * @param scoreArr
+     * @return
+     */
     public boolean validateIsZero(int[] scoreArr) {
         int checkZero = 0;
         for (int i=0; i<scoreArr.length; i++) {
