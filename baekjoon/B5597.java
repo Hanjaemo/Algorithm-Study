65449f3a1cd4dea6da7544e79275883f18304640
Hanjaemo <hjm2530@gmail.com>
2023-02-09 02:05:56 +0900

    solve

--- baekjoon/B5597.java
+++ baekjoon/B5597.java
@@ -6,15 +6,15 @@ class B5597 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        boolean[] student = new boolean[31];
+        boolean[] students = new boolean[31];
 
         for (int i=1; i<=28; i++) {
             int success = scanner.nextInt();
-            student[success] = true;
+            students[success] = true;
         }
 
         for (int i=1; i<=30; i++) {
-            if (!student[i]) {
+            if (!students[i]) {
                 System.out.println(i);
             }
         }
